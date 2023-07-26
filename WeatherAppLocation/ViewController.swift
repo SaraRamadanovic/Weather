5a0e967b24bd0cf4f38e24236c71071d239ef8f1
SaraRamadanovic <sara.ramadanovic@gmail.com>
2023-07-26 10:25:26 +0200

    app design, constraints, controller updated and pods installed

---
+++ WeatherAppLocation/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Sara Ramadanovic on 24/07/2023.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    let gradientLayer = CAGradientLayer()
+    
+    let apiKey = "651bfbfd84a0f6c1e80ccd4e76990a45"
+    var lat = 11.3445
+    var lon = 104.33224
+    var activityIndicator:NVActivityIndicatorView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatiorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: view.frame.width-indicatiorSize/2, y: view.frame.height-indicatiorSize/2, width: indicatiorSize, height: indicatiorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.allowsBackgroundLocationUpdates = true
+
+        activityIndicator.startAnimating()
+        
+        
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setBlueGradientBackground()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        lat = location.coordinate.latitude
+        lon = location.coordinate.longitude
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+            response in
+            self.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value {
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                
+                self.locationLabel.text = jsonResponse["name"].stringValue
+                self.conditionImageView.image = UIImage(named: iconName)
+                self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+            }
+        }
+        
+    }
+    
+    
+    func setBlueGradientBackground(){
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
+    
+    func setGreyGradientBackground(){
+        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
+        let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottomColor]
+    }
+
+
+}
+
